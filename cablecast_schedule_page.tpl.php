69235000ceff4bb1aba8191dcfde30c51ee79bcd
Ray Tiley <raytiley@raytiley.com>
2010-12-08 05:24:24 +0000

    Fixed Permissions and moved schedule table generation to tpl file

--- cablecast_schedule_page.tpl.php
+++ cablecast_schedule_page.tpl.php
@@ -1,4 +1,5 @@
 <div type="text" id="cablecast_schedule_datepicker" style="float:right"></div>
+<!-- Set up jquery ui calendar -->
   <script>
   	$(function() {
   		$("#cablecast_schedule_datepicker").datepicker({
@@ -6,4 +7,12 @@
       });
   	});
   	</script>
-<?php print $schedule_table ?>
+<?php $schedule_table_header = array(t("Time"), t("Program Name")); ?>
+<?php $schedule_table = array() ?>
+<?php if($schedule_nodes): ?>
+	<?php foreach($schedule_nodes as $schedule_run): ?>
+		<?php $schedule_table[] = array(date("h:i", $schedule_run->start_time), l($schedule_run->title, "node/".$schedule_run->cablecast_show_nid)) ?>
+	<?php endforeach ?>
+	
+	<?php print theme_table($schedule_table_header, $schedule_table) ?>
+<?php endif ?>
